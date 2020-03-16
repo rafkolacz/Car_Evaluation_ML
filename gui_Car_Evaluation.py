ba81775bf64bc0bfdeb523b4dc21d7baaf33a7b1
rafal <rafalrobertkolacz@gmail.com>
2020-03-16 15:25:48 +0100

    todo comboboxes

--- gui_Car_Evaluation.py
+++ gui_Car_Evaluation.py
@@ -10,37 +10,54 @@ class Root(Tk):
         self.minsize(400,200)
         self.welcomeLabel = ttk.Label(self, text="Hello and welcome in Car Evaluation App")
         self.welcomeLabel.grid(column=0, row=0)
-        for i in range(5):
+        for i in range(6):
             self.InitUI(i)
+            print(i)
         self.Button()
 
 
     def calculate(self):
         self.result = ttk.Label(self, text="Car class is " + self.value.get())
+        data = [self.valueB.get(),self.valueM.get(),self.valueD.get(),self.valueP.get(),self.valueL.get(),self.valueS.get()]
+        print(data)
+        print(self.valueS.get())
         self.result.grid(column=1, row=8)
         # print(Car_Evaluation.engine(data))
 
     def InitUI(self, row):
         self.value = StringVar()
+        self.valueB = StringVar()
+        self.valueD = StringVar()
+        self.valueP = StringVar()
+        self.valueM = StringVar()
+        self.valueL = StringVar()
+        self.valueS = StringVar()
+
         self.combo = ttk.Combobox(self, width=15, textvariable=self.value)
         if row == 0 or row == 1:
             self.combo['values'] = ('low', 'med', 'high', 'vhigh')
             if row == 0:
                 name = "buying price"
+                self.combo.configure(textvariable=self.valueB)
             else:
                 name = "price of the maintenance"
+                self.combo.configure(textvariable=self.valueM)
         if row == 2 or row == 3:
             self.combo['values'] = (2, 3, 4, 5, 'more')
             if row == 2:
                 name = "number of doors"
+                self.combo.configure(textvariable=self.valueD)
             else:
                 name = "capacity in terms of persons to carry"
+                self.combo.configure(textvariable=self.valueP)
         if row == 4:
             self.combo['values'] = ('small', 'med', 'big')
             name = "size of luggage boot"
+            self.combo.configure(textvariable=self.valueL)
         if row == 5:
             self.combo['values'] = ('low', 'med', 'high')
             name = "estimated safety of the car"
+            self.combo.configure(textvariable=self.valueS)
 
         self.combo.grid(column=1, row=row+1)
         self.label = ttk.Label(self, text = "Select " + name)
